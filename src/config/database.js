f734252c64aeda0a91d120f2d9f4fbc736cf5139
Murilo Blanco Flor <muriloflor@hotmail.com>
2020-04-22 18:21:02 -0400

    Database config

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'postgres-compose', // IP do postgres entre containers é o nome do container
+  host: 'localhost', // IP do postgres entre containers é o nome do container
   username: 'postgres',
   password: 'docker',
   database: 'base',
